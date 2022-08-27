c2bf96af8f72468866922cb3a8154d436137820d
Aurelien Arino <aurelien.arino@gmail.com>
2022-08-27 15:20:20 +0200

    improve install and switch in zsh_profile

--- install.sh
+++ install.sh
@@ -10,27 +10,27 @@ CONFIG=$HOME/.config
 [[ -d $CONFIG/htop ]] || mkdir $CONFIG/htop
 
 echo "Linking zsh"
-[[ -a ~/.zshrc ]] || ln -s $DOTFILES/zsh/.zshrc ~/.zshrc
-[[ -a ~/.zsh_profile ]] || ln -s $DOTFILES/zsh/.zsh_profile ~/.zsh_profile
-[[ -a ~/.zlogin ]] || ln -s $DOTFILES/zsh/.zlogin ~/.zlogin
-[[ -a ~/.p10k.zsh ]] || ln -s $DOTFILES/zsh/.p10k.zsh ~/.p10k.zsh
+[[ -f ~/.zshrc ]] || ln -s $DOTFILES/zsh/.zshrc ~/.zshrc
+[[ -f ~/.zsh_profile ]] || ln -s $DOTFILES/zsh/.zsh_profile ~/.zsh_profile
+[[ -f ~/.zlogin ]] || ln -s $DOTFILES/zsh/.zlogin ~/.zlogin
+[[ -f ~/.p10k.zsh ]] || ln -s $DOTFILES/zsh/.p10k.zsh ~/.p10k.zsh
 touch ~/.hushlogin
 
 echo "Linking nvim"
 [[ -d $CONFIG/nvim ]] || ln -s $DOTFILES/nvim $CONFIG/nvim
 
 echo "Linking tmux"
-[[ -a ~/.tmux.conf ]] || ln -s $DOTFILES/tmux/.tmux.conf ~/.tmux.conf
+[[ -f ~/.tmux.conf ]] || ln -s $DOTFILES/tmux/.tmux.conf ~/.tmux.conf
 
 echo "Linking personal todo"
-[[ -a ~/.personal/todo.md ]] || ln -s $DOTFILES/personal/todo.md ~/.personal/todo.md
+[[ -f ~/.personal/todo.md ]] || ln -s $DOTFILES/personal/todo.md ~/.personal/todo.md
 
 echo "Linking git config"
-[[ -a ~/.gitconfig ]] || ln -s $DOTFILES/git/.gitconfig ~/.gitconfig
-[[ -a ~/.gitignore ]] || ln -s $DOTFILES/git/.gitignore ~/.gitignore
+[[ -f ~/.gitconfig ]] || ln -s $DOTFILES/git/.gitconfig ~/.gitconfig
+[[ -f ~/.gitignore ]] || ln -s $DOTFILES/git/.gitignore ~/.gitignore
 
 echo "Linking alacritty"
-[[ -a $CONFIG/alacritty/alacritty.yml ]] || ln -s $DOTFILES/term/alacritty/alacritty.yml $CONFIG/alacritty/alacritty.yml
+[[ -f $CONFIG/alacritty/alacritty.yml ]] || ln -s $DOTFILES/term/alacritty/alacritty.yml $CONFIG/alacritty/alacritty.yml
 
 echo "Linking htop"
 ln -s $DOTFILES/htop/htoprc $CONFIG/htop/htoprc
