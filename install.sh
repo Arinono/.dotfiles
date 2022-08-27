0c2ca04bfba1a08acb0472b69ef180505293db0a
Aurelien Arino <aurelien.arino@gmail.com>
2022-08-27 09:05:41 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/env bash
 
+DOTFILES=$HOME/.dotfiles
+CONFIG=$HOME/.config
+
 [[ -d ~/.personal ]] || mkdir ~/.personal
 [[ -d ~/.ssh ]] || mkdir ~/.ssh
-[[ -d ~/.config ]] || mkdir ~/.config
-
-DOTFILES=$HOME/.dotfiles
+[[ -d $CONFIG ]] || mkdir $CONFIG
+[[ -d $CONFIG/alacritty ]] || mkdir $CONFIG/alacritty
+[[ -d $CONFIG/htop ]] || mkdir $CONFIG/htop
 
 echo "Linking zsh"
-ln -s $DOTFILES/zsh/.zshrc ~/.zshrc
-ln -s $DOTFILES/zsh/.zsh_profile ~/.zsh_profile
-ln -s $DOTFILES/zsh/.zlogin ~/.zlogin
-ln -s $DOTFILES/zsh/.p10k.zsh ~/.p10k.zsh
+[[ -a ~/.zshrc ]] || ln -s $DOTFILES/zsh/.zshrc ~/.zshrc
+[[ -a ~/.zsh_profile ]] || ln -s $DOTFILES/zsh/.zsh_profile ~/.zsh_profile
+[[ -a ~/.zlogin ]] || ln -s $DOTFILES/zsh/.zlogin ~/.zlogin
+[[ -a ~/.p10k.zsh ]] || ln -s $DOTFILES/zsh/.p10k.zsh ~/.p10k.zsh
 touch ~/.hushlogin
 
 echo "Linking nvim"
-ln -s $DOTFILES/nvim ~/.config/nvim
+[[ -d $CONFIG/nvim ]] || ln -s $DOTFILES/nvim $CONFIG/nvim
 
 echo "Linking tmux"
-ln -s $DOTFILES/tmux/.tmux.conf ~/.tmux.conf
+[[ -a ~/.tmux.conf ]] || ln -s $DOTFILES/tmux/.tmux.conf ~/.tmux.conf
 
 echo "Linking personal todo"
-ln -s $DOTFILES/personal/todo.md ~/.personal/todo.md
+[[ -a ~/.personal/todo.md ]] || ln -s $DOTFILES/personal/todo.md ~/.personal/todo.md
 
 echo "Linking git config"
-ln -s $DOTFILES/git/.gitconfig ~/.gitconfig
-ln -s $DOTFILES/git/.gitignore ~/.gitignore
+[[ -a ~/.gitconfig ]] || ln -s $DOTFILES/git/.gitconfig ~/.gitconfig
+[[ -a ~/.gitignore ]] || ln -s $DOTFILES/git/.gitignore ~/.gitignore
+
+echo "Linking alacritty"
+[[ -a $CONFIG/alacritty/alacritty.yml ]] || ln -s $DOTFILES/term/alacritty/alacritty.yml $CONFIG/alacritty/alacritty.yml
+
+echo "Linking htop"
+ln -s $DOTFILES/htop/htoprc $CONFIG/htop/htoprc
