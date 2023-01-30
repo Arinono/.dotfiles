1af5eaecf4e76bd8a9e138cc4e870e163b2b48c4
Aurelien Arino <aurelien.arino@gmail.com>
2023-01-30 10:39:01 +0100

    nvim(telescope): clean and add live_grep

--- nvim/after/plugin/keymap/telescope.lua
+++ nvim/after/plugin/keymap/telescope.lua
@@ -1,47 +1,38 @@
 local nnoremap = require("arinono.keymap").nnoremap
+local builtin = require('telescope.builtin')
 
 -- all files
-nnoremap("<leader>pf", function()
-  require("telescope.builtin").find_files()
-end)
+nnoremap("<leader>pf", builtin.find_files)
 
 nnoremap("<leader>pF", function()
-  require("telescope.builtin").find_files({ hidden = true })
+  builtin.find_files({ hidden = true })
 end)
 
 -- git files
-nnoremap("<leader>pg", function()
-  require("telescope.builtin").git_files()
-end)
+nnoremap("<leader>pg", builtin.git_files)
 
 nnoremap("<leader>pG", function()
-  require("telescope.builtin").git_files({ hidden = true })
+  builtin.git_files({ hidden = true })
 end)
 
 -- greped files with input
 nnoremap("<leader>ps", function()
-  require('telescope.builtin').grep_string({ search = vim.fn.input("🔭 > ")})
+  builtin.grep_string({ search = vim.fn.input("🔭 > ")})
 end)
+
+nnoremap("<leader>pls", builtin.live_grep)
 
 -- greped files with word under cursor
 nnoremap("<leader>pw", function()
-    require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }
+    builtin.grep_string { search = vim.fn.expand("<cword>") }
 end)
 
 -- buffers
-nnoremap("<leader>pb", function()
-  require("telescope.builtin").buffers()
-end)
+nnoremap("<leader>pb", builtin.buffers)
 
 -- help tags
-nnoremap("<leader>ht", function()
-  require("telescope.builtin").help_tags()
-end)
+nnoremap("<leader>ht", builtin.help_tags)
 
-nnoremap("<leader>sd", function()
-  require("telescope.builtin").diagnostics()
-end)
+nnoremap("<leader>sd", builtin.diagnostics)
 
-nnoremap("<leader>pk", function()
-  require("telescope.builtin").keymaps()
-end)
+nnoremap("<leader>pk", builtin.keymaps)
