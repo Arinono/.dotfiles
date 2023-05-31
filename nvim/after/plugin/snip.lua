068087de9319b3e7fb7fa64f3b5489a651b79caa
Aurelien Arino <aurelien.arino@gmail.com>
2023-05-31 09:44:14 +0200

    nvim: vue composable snips

--- nvim/after/plugin/snip.lua
+++ nvim/after/plugin/snip.lua
@@ -1,20 +1,41 @@
-local keymap = require('arinono.keymap')
-local ls = require('luasnip')
+local keymap = require("arinono.keymap")
+local ls = require("luasnip")
+
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local isn = ls.indent_snippet_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.conditions")
+local conds_expand = require("luasnip.extras.conditions.expand")
 
 local nnoremap = keymap.nnoremap
 local snoremap = keymap.snoremap
 local inoremap = keymap.inoremap
 
 function expand_or_jump_fwd()
-  if ls.expand_or_jumpable() then
-    ls.expand_or_jump()
-  end
+	if ls.expand_or_jumpable() then
+		ls.expand_or_jump()
+	end
 end
 
 function expand_or_jump_bwd()
-  if ls.jumpable(-1) then
-    ls.jump(-1)
-  end
+	if ls.jumpable(-1) then
+		ls.jump(-1)
+	end
 end
 
 nnoremap("<C-k>", expand_or_jump_fwd, { silent = true })
@@ -22,28 +43,67 @@ snoremap("<C-k>", expand_or_jump_fwd, { silent = true })
 nnoremap("<C-j>", expand_or_jump_bwd, { silent = true })
 snoremap("<C-j>", expand_or_jump_bwd, { silent = true })
 inoremap("<C-l>", function()
-  if ls.choice_active() then
-    ls.change_choice(1)
-  end
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
 end, { silent = true })
 
 nnoremap("<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/snip.lua<cr>")
 
 if vim.g.snippets == "luasnip" then
-  return
+	return
 end
 
 ls.config.set_config({
-  history = true,
+	history = true,
 
-  updateevents = "TextChanged,TextChangedI",
-  enable_autosnippet = false,
+	updateevents = "TextChanged,TextChangedI",
+	enable_autosnippet = false,
 })
 
-ls.snippets = {
-  all = {
-  },
+ls.cleanup()
 
-  vue = {
-  },
-}
+ls.add_snippets("all", {
+	ls.parser.parse_snippet({ trig = "te", wordTrig = false }, "${1:cond} ? ${2:lhs} : ${3:rhs}"),
+})
+
+ls.add_snippets("vue", {
+	s("uuid", t("const { uuid } = useUUID();")),
+	s("ret_uuid", t({ "return {", "  uuid,", "};" })),
+	s("title", {
+		isn(
+			1,
+			t({
+				"const { uuid } = useUUID();",
+				"const { title } = useTitle({ uuid });",
+			}),
+			"$PARENT_INDENT"
+		),
+	}),
+	s("i18n", {
+		isn(
+			1,
+			t({
+				"const { t } = useI18n({",
+				"  messages: ${1:json}",
+				"});",
+			}),
+			"$PARENT_INDENT"
+		),
+	}),
+	s("stp", {
+		isn(
+			1,
+			t({
+				"setup() {",
+				"  const { uuid } = useUUID();",
+				"",
+				"  return {",
+				"    uuid,",
+				"  };",
+				"},",
+			}),
+			"$PARENT_INDENT"
+		),
+	}),
+})
