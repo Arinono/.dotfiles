849680f24ec6066f456e23b5c699b9cb35860c77
Aurelien Arino <aurelien.arino@gmail.com>
2023-06-14 13:38:19 +0200

    nvim: quote/unquote

--- nvim/after/plugin/keymap/init.lua
+++ nvim/after/plugin/keymap/init.lua
@@ -9,7 +9,9 @@ nnoremap("<leader>t", ":tabnew<cr>")
 nnoremap("Q", "<nop>")
 
 -- Neoformat
-nnoremap("<leader>f", function() vim.lsp.buf.format() end)
+nnoremap("<leader>f", function()
+	vim.lsp.buf.format()
+end)
 
 -- Files
 nnoremap("<leader>e", "<cmd>Ex<cr>")
@@ -17,8 +19,11 @@ nnoremap("<leader>e", "<cmd>Ex<cr>")
 -- Edition
 vnoremap("J", ":m '>+1<CR>gv=gv")
 vnoremap("K", ":m '<-2<CR>gv=gv")
-xnoremap("<leader>p", "\"_dP")
-nnoremap('J', 'mzJ`z')
+xnoremap("<leader>p", '"_dP')
+nnoremap("J", "mzJ`z")
+
+nnoremap("<leader>'", "ciw'<C-r>\"'<esc>")
+nnoremap("<leader>;'", "di'hPl2x")
 
 -- Navigation
 nnoremap("<C-d>", "<C-d>zz")
@@ -28,9 +33,9 @@ nnoremap("N", "Nzz")
 nnoremap("G", "Gzz")
 
 -- Crossyank
-nnoremap('<leader>y', '"+y')
-vnoremap('<leader>y', '"+y')
-nnoremap('<leader>Y', '"+Y')
+nnoremap("<leader>y", '"+y')
+vnoremap("<leader>y", '"+y')
+nnoremap("<leader>Y", '"+Y')
 
 -- Commands
 nnoremap("<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
