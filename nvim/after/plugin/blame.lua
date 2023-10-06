334397e1cbcf11ea9008de6ec4315cfe5854965e
Aurelien Arino <aurelien@arino.io>
2023-10-06 10:51:37 +0200

    nvim: fmt blame

--- nvim/after/plugin/blame.lua
+++ nvim/after/plugin/blame.lua
@@ -1,6 +1,6 @@
-local nnoremap = require('arinono.keymap').nnoremap
-local git_blame = require('gitblame')
-local lualine = require('lualine')
+local nnoremap = require("arinono.keymap").nnoremap
+local git_blame = require("gitblame")
+local lualine = require("lualine")
 
 vim.g.gitblame_enabled = 0
 vim.g.gitblame_message_template = "<author> | <date> | <summary>"
@@ -10,34 +10,41 @@ vim.g.gitblame_display_virtual_text = 0
 
 local show = false
 local function update(text)
-  lualine.setup({
-    sections = {
-      lualine_x = { text }
-    }
-  })
+	lualine.setup({
+		sections = {
+			lualine_x = { text },
+		},
+	})
 
-  lualine.refresh()
+	lualine.refresh()
 end
 
 update("")
 
 nnoremap("<leader>bt", function()
-  vim.api.nvim_call_function("GitBlameToggle", {})
+	vim.api.nvim_call_function("GitBlameToggle", {})
 
-  if not show then
-    update({
-      git_blame.get_current_blame_text,
-      cond = git_blame.is_blame_text_available,
-    })
-    show = true
-  else
-    update("")
-    show = false
-  end
-
+	if not show then
+		update({
+			git_blame.get_current_blame_text,
+			cond = git_blame.is_blame_text_available,
+		})
+		show = true
+	else
+		update("")
+		show = false
+	end
 end, { silent = false })
 
-nnoremap("<leader>boc", function() vim.api.nvim_call_function("GitBlameOpenCommitURL", {}) end, { silent = false })
-nnoremap("<leader>bof", function() vim.api.nvim_call_function("GitBlameOpenFileURL", {}) end, { silent = false })
-nnoremap("<leader>bcc", function() vim.api.nvim_call_function("GitBlameCopyCommitURL", {}) end, { silent = false })
-nnoremap("<leader>bcf", function() vim.api.nvim_call_function("GitBlameCopyFileURL", {}) end, { silent = false })
+nnoremap("<leader>boc", function()
+	vim.api.nvim_call_function("GitBlameOpenCommitURL", {})
+end, { silent = false })
+nnoremap("<leader>bof", function()
+	vim.api.nvim_call_function("GitBlameOpenFileURL", {})
+end, { silent = false })
+nnoremap("<leader>bcc", function()
+	vim.api.nvim_call_function("GitBlameCopyCommitURL", {})
+end, { silent = false })
+nnoremap("<leader>bcf", function()
+	vim.api.nvim_call_function("GitBlameCopyFileURL", {})
+end, { silent = false })
