2c33c51745b43fe094325c8b77a7e1c5dc5572c4
Aurelien Arino <aurelien@arino.io>
2024-03-16 10:20:08 +0100

    Update init.lua

--- nvim/lua/arinono/init.lua
+++ nvim/lua/arinono/init.lua
@@ -3,3 +3,26 @@ require("arinono.packer")
 
 require("arinono.wtg")
 require("arinono.toggle_theme")
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+local arinono_group = augroup('Arinono', {})
+local yank_group = augroup('HighlightYank', {})
+
+autocmd({"BufWritePre"}, {
+    group = arinono_group,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
