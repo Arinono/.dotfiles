e265fccbe33c4a510a625adc9c31386362bfd2c8
Aurelien Arino <aurelien@arino.io>
2024-09-11 14:10:15 +0200

    wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,10 +1,13 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
+
 config.automatically_reload_config = true
+config.quit_when_all_windows_are_closed = true
 
 config.color_scheme = "tokyonight_storm"
 config.font_size = 14
+config.freetype_load_flags = "NO_HINTING"
 config.font = wezterm.font_with_fallback({
 	{ family = "Dank Mono", weight = "Bold" },
 	{ family = "MesloLGL Nerd Font", weight = "Bold" },
