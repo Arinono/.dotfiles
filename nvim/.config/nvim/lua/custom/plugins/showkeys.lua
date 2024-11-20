734e58638fa7f5820f003a3c42d8b39d6831274e
Aurelien Arino <aurelien@arino.io>
2024-11-20 10:28:53 +0100

    nvim: showkeys

---
+++ nvim/.config/nvim/lua/custom/plugins/showkeys.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvzone/showkeys",
+    cmd = "ShowkeysToggle",
+    lazy = true,
+    opts = {
+      timeout = 1,
+      maxkeys = 5,
+      position = "top-center",
+    },
+  },
+}
