968dcd39606413ac9bf25a0c6ab3094c7504dd9d
Aurelien Arino <aurelien@arino.io>
2024-11-26 17:38:27 +0100

    smaller sketchybar

--- sketchybar/.config/sketchybar/items/spaces.sh
+++ sketchybar/.config/sketchybar/items/spaces.sh
@@ -7,19 +7,18 @@ for sid in $(aerospace list-workspaces --all); do
         --subscribe "space.$sid" aerospace_workspace_change \
         --set "space.$sid" \
         icon="$sid"\
-                              icon.padding_left=22                          \
-                              icon.padding_right=22                         \
-                              label.padding_right=33                        \
+                              icon.padding_left=8                          \
+                              icon.padding_right=8                         \
                               icon.highlight_color=$RED                     \
                               background.color=0x44ffffff \
                               background.corner_radius=5 \
-                              background.height=30 \
+                              background.height=20 \
                               background.drawing=off                         \
                               label.font="sketchybar-app-font:Regular:16.0" \
-                              label.background.height=30                    \
+                              label.background.height=20                    \
                               label.background.drawing=on                   \
                               label.background.color=0xff494d64             \
-                              label.background.corner_radius=9              \
+                              label.background.corner_radius=5              \
                               label.drawing=off                             \
         click_script="aerospace workspace $sid" \
         script="$CONFIG_DIR/plugins/aerospacer.sh $sid"
