f1ec02a61810362227065f3398bd36b22052f8f0
Aurelien Arino <dev@arino.io>
2025-07-04 13:49:35 +0200

    fix bluetooth sinks

--- nix/machines/viktor/default.nix
+++ nix/machines/viktor/default.nix
@@ -100,14 +100,14 @@
   services.pipewire = {
     enable = true;
     alsa.enable = true;
-    alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
     wireplumber.extraConfig.bluetoothEnhancements = {
       "monitor.bluez.properties" = {
         "bluez5.enable-sbc-xq" = true;
         "bluez5.enable-msbc" = true;
         "bluez5.enable-hw-volume" = true;
-        "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+        "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" "a2dp_sink" "a2dp_source"];
       };
     };
   };
@@ -117,6 +117,7 @@
     settings = {
       General = {
         Enable = "Source,Sink,Media,Socket";
+        ControllerMode = "dual";
         Experimental = true; # Enables newer features
       };
     };
