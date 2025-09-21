ad8e1358636d8ddbbc5c946cb3d2e17a8a626c79
Aurelien Arino <dev@arino.io>
2025-09-21 12:22:59 +0200

    claude code on viktor

--- nix/machines/viktor/default.nix
+++ nix/machines/viktor/default.nix
@@ -132,6 +132,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    claude-code
     obs-studio
     discord-ptb
     # framework
