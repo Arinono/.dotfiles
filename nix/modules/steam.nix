2f9482f5a7e635881b15d912c964b5df3cabb5b1
Aurelien Arino <dev@arino.io>
2026-02-26 18:45:05 +0100

    steam unfree

--- nix/modules/steam.nix
+++ nix/modules/steam.nix
@@ -1,16 +1,28 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = false;
     dedicatedServer.openFirewall = false;
     localNetworkGameTransfers.openFirewall = true;
-    gamescopeSession.enable = true;
+    # gamescopeSession.enable = true;
   };
 
-  programs.gamescope = {
-    enable = true;
-    capSysNice = true;
-  };
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "steam"
+      "steam-original"
+      "steam-unwrapped"
+      "steam-run"
+    ];
+
+  # programs.gamescope = {
+  #   enable = true;
+  #   capSysNice = true;
+  # };
 
   environment.systemPackages = with pkgs; [
     steam
