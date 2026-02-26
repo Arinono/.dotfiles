e874a32cab4e4246a55a5183e96aae83b17f757b
Aurelien Arino <dev@arino.io>
2026-02-26 18:49:47 +0100

    clean steam (not working ?)

--- nix/modules/steam.nix
+++ nix/modules/steam.nix
@@ -1,28 +1,16 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = false;
     dedicatedServer.openFirewall = false;
     localNetworkGameTransfers.openFirewall = true;
-    # gamescopeSession.enable = true;
+    gamescopeSession.enable = true;
   };
 
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "steam"
-      "steam-original"
-      "steam-unwrapped"
-      "steam-run"
-    ];
-
-  # programs.gamescope = {
-  #   enable = true;
-  #   capSysNice = true;
-  # };
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
 
   environment.systemPackages = with pkgs; [
     steam
