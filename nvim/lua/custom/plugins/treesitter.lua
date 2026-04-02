3bdccc3a971a3c1c902e2a59dfa5a098097faf61
Aurelien Arino <aurelien@withthegrid.com>
2026-04-02 11:41:06 +0200

    nvim: update treesitter to main for nvim 12

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-local installed = {
+local parsers = {
   "vim",
   "c",
   "bash",
@@ -38,27 +38,36 @@ local context = {
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  branch = "main",
+  lazy = false,
   opts = {
-    ensure_installed = installed,
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    sync = false,
-    highlight = {
-      enable = true,
-      disable = { "help" },
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true },
-  },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-context",
-    opts = context,
-    "nvim-treesitter/playground",
+    ensure_installed = parsers,
   },
   config = function(_, opts)
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup(opts)
+    -- require("nvim-treesitter").install(parsers)
+    -- vim.treesitter.language.register("markdown", { "mdx" })
 
-    vim.treesitter.language.register("markdown", { "mdx" })
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        local buf, filetype = args.buf, args.match
+
+        local language = vim.treesitter.language.get_lang(filetype)
+        if not language then
+          return
+        end
+
+
+        -- check if parser exists and load it
+        if not vim.treesitter.language.add(language) then
+          return
+        end
+
+        -- enables syntax highlighting and other treesitter features
+        vim.treesitter.start(buf, language)
+
+        -- enables treesitter based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
   end,
 }
