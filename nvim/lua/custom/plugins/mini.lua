15b67827aef141edc14e032f7138347d230b90eb
Aurelien Arino <aurelien@withthegrid.com>
2026-05-01 13:35:11 +0200

    nvim: switch comment plugin

--- nvim/lua/custom/plugins/mini.lua
+++ nvim/lua/custom/plugins/mini.lua
@@ -1,17 +1,20 @@
 return {
-  "echasnovski/mini.nvim",
-  config = function()
-    require("mini.surround").setup()
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      require("mini.surround").setup()
+      require("mini.comment").setup()
 
-    local statusline = require("mini.statusline")
-    statusline.setup({ use_icons = vim.g.have_nerd_font })
+      local statusline = require("mini.statusline")
+      statusline.setup({ use_icons = vim.g.have_nerd_font })
 
-    --- @diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return "%2l:%-2v"
-    end
+      --- @diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return "%2l:%-2v"
+      end
 
-    -- ... and there is more!
-    --  Check out: https://github.com/echasnovski/mini.nvim
-  end,
+      -- ... and there is more!
+      --  Check out: https://github.com/echasnovski/mini.nvim
+    end,
+  },
 }
